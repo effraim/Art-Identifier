343fc42b0537b748b5c096fa96a4a1e470900ba4
effraim <32572199+effraim@users.noreply.github.com>
2022-09-01 08:01:59 +0300

    Add files via upload

---
+++ Website/ArtIdentifier/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ArtIdentifier project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ArtIdentifier.settings')
+
+application = get_asgi_application()
